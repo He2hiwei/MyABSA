adc31bbf502b076054d9a0fba3f979c33b4528cd
NDHoHo <2832079925@qq.com>
2018-10-05 23:35:31 +0800

    use parse

--- train_model.py
+++ train_model.py
@@ -4,61 +4,64 @@ import pandas as pd
 import numpy as np
 import json
 import re 
+import argparse
 from gensim import corpora
 from cnn_gate_aspect_model_acsa import CNN_Gate_Aspect_Text
 from torch.autograd import Variable
 from utils import glove 
 
+parser = argparse.ArgumentParser(description='CNN text classificer')
+
+# learning
+parser.add_argument('-lr', type=float, default=0.01, help='initial learning rate [default: 0.001]')
+parser.add_argument('-l2', type=float, default=0, help='initial learning rate [default: 0]')
+parser.add_argument('-momentum', type=float, default=0.99, help='SGD momentum [default: 0.99]')
+parser.add_argument('-epochs', type=int, default=30, help='number of epochs for train [default: 30]')
+parser.add_argument('-batch-size', type=int, default=32, help='batch size for training [default: 32]')
+parser.add_argument('-grad_clip', type=float, default=5, help='max value of gradients')
+parser.add_argument('-lr_decay', type=float, default=0, help='learning rate decay')
+parser.add_argument('-seed', type=int, default=1, help='random seed number [default: 1]')
+
+# model CNNs
+parser.add_argument('-embed-dim', type=int, default=300, help='number of embedding dimension [default: 300]')
+parser.add_argument('-unif', type=float, help='Initializer bounds for embeddings', default=0.25)
+parser.add_argument('-shuffle', action='store_true', default=True, help='shuffle the data every epoch [default: True]')
+parser.add_argument('-kernel_num', type=int, default=100, help='number of the kernel [default: 100]')
+parser.add_argument('-kernel_sizes', type=list, default=[3,4,5], help='sizes of conv kenerl [default: [3,4,5]]')
+
+# task
+parser.add_argument('-atsa', action='store_const', default='acsa', const='atsa', help='conduct atsa task [default: "atsa"]')
+
+# dataset
+parser.add_argument('-hard', action='store_const', default='', const='_hard',help='use hard date [default: ""]')
+parser.add_argument('-large', action='store_const', default='-2014', const='-large',help='use large date [default: "-2014"]')
+
+
+args = parser.parse_args()
 
 PROJECT_PATH = '/Users/apple/github/my-GCAE-master'
 Glove_PATH = '/Users/apple/AVEC2017/utils/datasets/glove.840B.300d.txt'
-EMBED_DIM = 300
-#SENTENCE_INPUT_LENGTH = 50
 
-class Parameter:
-    def __init__(self):
-        self.embed_num = 100
-        self.embed_dim = 10
-        self.class_num = 10
-        self.aspect_num = 10
-#        self.aspect_embed_dim = 10
-        
-        self.embedding = torch.randn(100,10)
-        self.aspect_embedding = torch.randn(10,10)
-
-        self.kernel_num = 10
-        self.kernel_sizes = [2,3,4]
-
-# ## data processing
-# restaurant_train  = pd.read_json(PROJECT_PATH + '/acsa-restaurant-2014/acsa_train.json')
-# restaurant_test  = pd.read_json(PROJECT_PATH + '/acsa-restaurant-2014/acsa_test.json')
-
-# restaurant_train  = pd.read_json(PROJECT_PATH + '/acsa-restaurant-2014/acsa_hard_train.json')
-# restaurant_test  = pd.read_json(PROJECT_PATH + '/acsa-restaurant-2014/acsa_hard_test.json')
-
-
-# restaurant_train  = pd.read_json(PROJECT_PATH + '/acsa-restaurant-large/acsa_train.json')
-# restaurant_test  = pd.read_json(PROJECT_PATH + '/acsa-restaurant-large/acsa_test.json')
-        
-restaurant_train  = pd.read_json(PROJECT_PATH + '/acsa-restaurant-large/acsa_hard_train.json')
-restaurant_test  = pd.read_json(PROJECT_PATH + '/acsa-restaurant-large/acsa_hard_test.json')
-
-
-cnf_titile = 'acsa-restaurant-hard'
+ ## data processing
+restaurant_train  = pd.read_json(PROJECT_PATH + '/acsa-restaurant' + args.large +'/acsa' + args.hard + '_train.json')
+restaurant_test  = pd.read_json(PROJECT_PATH + '/acsa-restaurant' + args.large +'/acsa' + args.hard + '_test.json')
+cnf_titile = 'acsa-restaurant' + args.large + args.hard
+print("json data: " + cnf_titile + " is loaded")
 
 # ---------------
-# new code
 from utils import absa_text2num
 import torch.utils.data as Data
 
+print('text is changing to numeric representation...')
 embed_lookup_tab, train_sentence, \
 train_aspect, train_senti, \
 test_sentence, test_aspect, \
-test_senti, senti_token2id_dict  = absa_text2num.absa_text2num(restaurant_train,restaurant_test)
+test_senti, senti_token2id_dict  = absa_text2num.absa_text2num(restaurant_train,restaurant_test, 
+                                                            Glove_PATH=Glove_PATH, EMBED_DIM=args.embed_dim)
 
 
 
-embed_lookup_tab_zero_pad = np.concatenate((embed_lookup_tab, np.zeros((1,EMBED_DIM))),axis=0)
+embed_lookup_tab_zero_pad = np.concatenate((embed_lookup_tab, np.zeros((1,args.embed_dim))),axis=0)
 
 # Convert list of lists with different lengths to a numpy array
 
@@ -75,24 +78,17 @@ for i,j in enumerate(test_sentence):
     test_sentence_array[i][0:len(j)] = j
 
 # parameter setting
-args = Parameter()
 args.embed_num = len(embed_lookup_tab_zero_pad) + 1 # the last row is zero padding(TODO: require_grad=False)
-args.embed_dim = EMBED_DIM
 args.class_num = train_senti.max() + 1
-
 args.aspect_num = len(np.unique(train_aspect))
-args.aspect_embed_dim = EMBED_DIM
+args.aspect_embed_dim =args.embed_dim
 
 args.embedding = torch.from_numpy(embed_lookup_tab_zero_pad) # embedding for sentence and aspect input
 #args.aspect_embedding = torch.from_numpy(aspect_glove_embeds)
 
-args.kernel_num = 100
-args.kernel_sizes = [3,4,5] # n-gram
 
+torch.manual_seed(args.seed)    # reproducible
 
-torch.manual_seed(1)    # reproducible
-
-BATCH_SIZE = 32
 
 x = torch.linspace(0, len(train_sentence)-1, len(train_sentence)).int()    # this is index of x data (torch tensor)
 y = torch.linspace(0, len(train_sentence)-1, len(train_sentence)).int()      # this is index of y data (torch tensor)
@@ -100,7 +96,7 @@ y = torch.linspace(0, len(train_sentence)-1, len(train_sentence)).int()      # t
 torch_dataset = Data.TensorDataset(x, y)
 loader = Data.DataLoader(
     dataset=torch_dataset,      # torch TensorDataset format
-    batch_size=BATCH_SIZE,      # mini batch size
+    batch_size=args.batch_size,      # mini batch size
     shuffle=True,               # random shuffle for training
     num_workers=2,              # subprocesses for loading data
 )
